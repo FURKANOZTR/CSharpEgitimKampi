1c0f80c25e80e03e7ab9d7c6e69e83e8f402c339
Muhammed Furkan OZTURK <furkanoztr34@hotmail.com>
2025-08-13 17:18:04 +0300

    ,,,

---
+++ 07_ForEachLoop/Program.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ForEachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ForEach Döngüsü
+
+            // ForEach döngüsü, bir koleksiyonun her bir elemanını teker teker işlemek için kullanılır.
+            // Bu döngü, özellikle diziler, listeler ve diğer koleksiyonlar üzerinde işlem yaparken oldukça kullanışlıdır.
+            // Foreach(1;2;3;4) parametre alır 1.Değişken türü 2.Değişken adı 3.In komutu 4.Liste, Kolesyon veya dizi
+
+            //string[] cities = { "Ankara", "İstanbul", "İzmir", "Bursa", "İstanbul" };
+
+            //foreach (string city in cities)
+            //{
+            //    Console.WriteLine(city);
+            //}
+
+            //int[] numbers = { 45, 245, 445, 896, 726, 784, 1407, 4555, 124 };
+
+            //foreach (int number in numbers)
+            //{
+            //    // Her bir sayının karesini hesaplayıp ekrana yazdırıyoruz.
+            //    Console.WriteLine($"Sayının Karesi: {number * number}");
+            //}
+
+            //foreach (int number in numbers)
+            //{
+            //    if (number % 2 == 0)
+            //    {
+            //        // Eğer sayı çift ise, ekrana çift sayıları yazdırıyoruz.
+            //        Console.WriteLine($"Çift Sayı: {number}");
+            //    }
+            //    else
+            //    {
+            //        // Eğer sayı tek ise, ekrana tek sayıları yazdırıyoruz.
+            //        Console.WriteLine($"Tek Sayı: {number}");
+            //    }
+            //}   
+
+            //int[] numbers = { 45, 245, 445, 896, 726, 784, 1407, 4555, 124 };
+            //int sum = 0;
+
+            //foreach (int number in numbers) 
+            //{ 
+            //    sum += number;
+            //}
+            //Console.WriteLine($"Toplam: {sum}");
+
+            //List<int> numbers = new List<int>(); // Liste oluşturma / Liste bir türdeki verileri saklamak için kullanılır.
+            //numbers.Add(45);
+            //numbers.Add(245);
+            //numbers.Add(445);
+            //numbers.Add(896);
+            //numbers.Add(726);
+            //numbers.Add(784); // listeye eleman ekleme
+
+            //List<string> cities = new List<string>() { "Ankara", "İstanbul", "İzmir", "Bursa", "İstanbul" }; // Bu şekilde de liste oluşturabiliriz.
+
+            //Console.WriteLine(numbers); // System.Collections.Generic.List`1[System.Int32] bu şekilde yazdırılır.
+
+            //foreach (int number in numbers) 
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //string word = "Merhaba Dünya"; // Bir kelime tanımlıyoruz.Ve bu kelimenin her bir harfini teker teker ekrana yazdıracağız.
+            ////stringler C# bir dizidir. Yani stringler de bir koleksiyondur.
+            //foreach (char letter in word)
+            //{
+            //    // Her bir harfi teker teker ekrana yazdırıyoruz.
+            //    Console.WriteLine(letter);
+            //}
+
+
+
+            #endregion
+
+            #region Tüm Konuların Tekrarı Örneği
+
+            //Console.Write("Lütfen ögrenci sayısını giriniz:");
+            //int studentCount = int.Parse(Console.ReadLine());
+
+            //string[] studentsInfo = new string[studentCount]; // Öğrenci sayısı kadar bir dizi oluşturuyoruz.
+            //int[] studentsNot = new int[studentCount];
+            //int totalGrade = 0; // Toplam notu tutmak için bir değişken tanımlıyoruz.
+            //int index1 = 0; // Dizi indeksini başlatıyoruz.
+            //int index2 = 0; // Dizi indeksini başlatıyoruz.
+
+            //foreach (string _ in studentsInfo) // _ işareti, değişkenin kullanılmadığını belirtir. Bu durumda sadece döngü için kullanıyoruz.
+            //{
+            //    Console.Write($"Lütfen {index1 + 1}.öğrencinin adını giriniz:");
+            //    string name = Console.ReadLine();
+            //    studentsInfo[index1] = name; // Her bir öğrencinin adını diziye ekliyoruz.
+            //    index1++; // İndeksi artırıyoruz.
+            //}
+
+            //foreach (int _ in studentsNot) // _ işareti, değişkenin kullanılmadığını belirtir. Bu durumda sadece döngü için kullanıyoruz.
+            //{
+            //    Console.Write($"Lütfen {index2 + 1}.öğrencinin notunu giriniz:");
+            //    int grade = int.Parse(Console.ReadLine());
+            //    studentsNot[index2] = grade; // Her bir öğrencinin notunu diziye ekliyoruz.
+            //    index2++; // İndeksi artırıyoruz.
+            //    totalGrade += grade; // Notları topluyoruz.
+            //}
+
+            //Console.WriteLine("----------Öğrenci Bilgileri ve Notları----------");
+
+            //for(int i = 0; i < studentCount; i++)
+            //{
+            //    // Öğrenci adını ve notunu ekrana yazdırıyoruz.
+            //    Console.WriteLine($"Öğrenci Adı: {studentsInfo[i]} / Not: {studentsNot[i]}");
+            //}
+
+            //int averageGrade = totalGrade / studentCount; // Sınıf ortalamasını hesaplıyoruz.
+
+            //Console.WriteLine($"Sınıf Ortalaması: {averageGrade}");
+
+
+            //Console.Write("Lütfen ögrenci sayısını giriniz:");
+            //int studentCount = int.Parse(Console.ReadLine());
+
+            //string[] studentsInfo = new string[studentCount]; // Öğrenci sayısı kadar bir dizi oluşturuyoruz.
+            //double[] studentsNot = new double[studentCount];
+            //double totalGrade = 0; // Toplam notu tutmak için bir değişken tanımlıyoruz.
+
+            //for(int i = 0; i < studentCount; i++)
+            //{
+            //    Console.Write($"Lütfen {i + 1}.öğrencinin adını giriniz:");
+            //    string name = Console.ReadLine();
+            //    studentsInfo[i] = name; // Her bir öğrencinin adını diziye ekliyoruz.
+            //}
+
+            //for (int i = 0; i < studentCount; i++)
+            //{
+            //    Console.Write($"Lütfen {i + 1}.öğrencinin notunu giriniz:");
+            //    double grade = double.Parse(Console.ReadLine());
+            //    studentsNot[i] = grade; // Her bir öğrencinin notunu diziye ekliyoruz.
+            //    totalGrade += grade; // Notları topluyoruz.
+            //}
+
+            //Console.WriteLine("----------Öğrenci Bilgileri ve Notları----------");
+
+            //for (int i = 0; i < studentCount; i++)
+            //{
+            //    // Öğrenci adını ve notunu ekrana yazdırıyoruz.
+            //    Console.WriteLine($"Öğrenci Adı: {studentsInfo[i]} / Not: {studentsNot[i]}");
+            //}
+
+            //double averageGrade = totalGrade / studentCount; // Sınıf ortalamasını hesaplıyoruz.
+
+            //Console.WriteLine($"Sınıf Ortalaması: {averageGrade}");
+
+
+
+            //Console.Write("Lütfen ögrenci sayısını giriniz:");
+            //int studentCount = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("-----------------------------------------------------");
+
+            //string[] studentsInfo = new string[studentCount]; // Öğrenci sayısı kadar bir dizi oluşturuyoruz.
+            //double[] studentsAverageNot = new double[studentCount];
+
+            //for (int i = 0; i < studentCount; i++)
+            //{
+            //    Console.Write($"Lütfen {i + 1}.öğrencinin adını giriniz:");
+            //    string name = Console.ReadLine();
+            //    studentsInfo[i] = name; // Her bir öğrencinin adını diziye ekliyoruz.
+            //    Console.WriteLine();
+            //    double totalGrade = 0; // Toplam notu tutmak için bir değişken tanımlıyoruz.Total notu her öğrenci için sıfırlıyoruz.
+
+            //    for (int j = 1; j <= 3; j++)
+            //    {
+            //        Console.Write($"Lütfen {studentsInfo[i]} adlı öğrencinin {j}.notunu giriniz: ");
+            //        double grade = double.Parse(Console.ReadLine());
+            //        totalGrade += grade; // Notları topluyoruz.
+            //    }
+            //    Console.WriteLine("-----------------------------------------------------");
+
+            //    studentsAverageNot[i] = totalGrade / 3; // Her öğrencinin not ortalamasını hesaplıyoruz.
+            //}
+
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine("----------Öğrenci Bilgileri ve Notları----------");
+
+            //for (int i = 0; i < studentCount; i++)
+            //{
+            //    Console.WriteLine("-----------------------------------------------------");
+            //    // Öğrenci adını ve notunu ekrana yazdırıyoruz.
+            //    Console.WriteLine($"Öğrenci Adı: {studentsInfo[i]} / Not Ortalaması: {studentsAverageNot[i]}");
+            //    if (studentsAverageNot[i] < 50)
+            //    {
+            //        Console.WriteLine($"{studentsInfo[i]} adlı öğrenci başarısız.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{studentsInfo[i]} adlı öğrenci başarılı.");
+            //    }
+            //    Console.WriteLine("-----------------------------------------------------");
+            //    Console.WriteLine();
+            //}
+
+            //double sum = 0;
+
+            //for (int i = 0; i<studentCount; i++)
+            //{
+            //    sum += studentsAverageNot[i]; 
+            //}
+            //double averageGrade = sum / studentCount; // Sınıf ortalamasını hesaplıyoruz.
+
+            //Console.WriteLine($"Sınıf Ortalaması: {averageGrade}");
+
+
+            #endregion
+        }
+    }
+}
---
+++ 08_Methods/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Void Methods
+            // Method alsında bir fonksiyon, bir işlevdir. C dilinde de fonksiyonlara benzer bir yapı vardır. C# Örnek Methods Parse bir metodudur.          
+            // Methodlar, kodun tekrarını önlemek ve kodu daha okunabilir hale getirir.Methodların sonunda () parantezler bulunur. Parantezler içinde parametreler bulunabilir.
+            // Methodlar C# da mor küp olarak görünür.
+            // 1-Geriye değer döndüren methodlar 2-Geriye değer döndürmeyen methodlar(Void) olarak ikiye ayrılır.
+
+            //void customerList() // Methodun üstüne gelince kilit işareti görünür bu methodun private olduğunu gösterir.
+            //{
+            //    Console.WriteLine("Ali Yıldız");
+            //    Console.WriteLine("Ayşe Yıldız");
+            //    Console.WriteLine("Veli Çınar");
+            //    Console.WriteLine("Hakan Öztürk"); //Ekrana bir şey yazdırmaz Çünkü customerList() metodu çağrılmadı.
+            //}
+
+            //customerList(); // customerList() metodu çağrıldı. Ekrana yazdırma işlemi gerçekleşti.
+
+            //Console.WriteLine();
+
+            //for(int i = 1; i <= 5; i++)
+            //{
+            //    customerList(); // customerList() metodu 5 kez çağrıldı. Ekrana yazdırma işlemi 5 kez gerçekleşti.
+            //}
+
+            //void Sum() // Mehodları adlandırırken PascalCase kullanılır. Yani ilk harf büyük, sonraki harfler küçük yazılır.
+            //{ 
+            //    int x = 10, y = 20;
+            //    int result = x + y;
+            //    Console.WriteLine("Toplam: " + result); // Ekrana toplamı yazdırır.
+            //}
+
+            //Sum(); // Sum() metodu çağrıldı. Ekrana toplamı yazdırma işlemi gerçekleşti.
+
+
+
+
+
+
+
+
+
+
+            #endregion
+
+            #region Geriye Değer Döndürmeyen String Parametreli Methodlar
+
+            //void Write(string customerName) // parametrenin ingilizcesi argumenttir. Parametreler methodların içine yazılır.
+            //{
+            //    Console.WriteLine($"Merhaba, C# Programlama Dili! ve Senin İsmin: {customerName}");
+            //}
+
+            //Write("Muhammed Furkan Öztürk");
+
+            //void CustomerCard(string name, string surname)
+            //{
+            //    Console.WriteLine($"Müşteri Kartı Oluşturuldu: {name} {surname}");
+            //}
+
+            ////CustomerCard(); // Böyle hata verir çünkü parametreler boş bırakıldı.iki parametre göndermelisiniz.
+            //CustomerCard("Nimet", "Öztürk");
+            //CustomerCard("Bekir", "Öztürk");
+
+            #endregion
+
+            #region Geriye Değer Döndürmeyen Int Parametreli Methodlar
+
+            //void Sum(int number1, int number2) // Methodun geri dönüş tipi int. Yani bu method bir int değer döndürecek.
+            //{
+            //    int result = number1 + number2; // return ifadesi ile geri dönüş yapılır. return ifadesi ile methoddan çıkılır.
+            //    Console.WriteLine($"Toplam: {result}"); // Ekrana toplamı yazdırır.
+            //}
+
+            //Sum(10, 20); // Sum() metodu çağrıldı. Ekrana toplamı yazdırma işlemi gerçekleşti.
+
+            #endregion
+
+            #region Geriye Değer Döndüren String Parametreli ve Parametresiz Methodlar
+
+            //string CustomerName(string name, string surname) // Methodun geri dönüş tipi string. Yani bu method bir string değer döndürecek.
+            //{
+            //    return $"Müşteri Adı: {name} {surname}"; // return ifadesi ile geri dönüş yapılır. return ifadesi ile methoddan çıkılır.
+            //}
+
+            //CustomerName("Furkan", "Öztürk"); //Bir karşılıgı olmaz sadece değeri geri dönderdi. Ekrana yazdırma işlemi gerçekleşmedi.
+
+            //string CustomerName(string name, string surname) 
+            //{
+            //    return $"Müşteri Adı: {name} {surname}"; 
+            //}
+
+            //Console.WriteLine(CustomerName("Furkan", "Öztürk")); // Böyle yaparsak ekrana yazdırma işlemi gerçekleşir. Çünkü CustomerName() metodu çağrıldı
+            //                                                     // ve geri dönüş değeri Console.WriteLine() ile ekrana yazdırıldı.
+
+            //string StudentCard()
+            //{
+            //    string name = "Furkan";
+            //    string surname = "Öztürk";
+
+            //    return $"{name} {surname}";
+            //}
+
+            //Console.WriteLine(StudentCard()); // StudentCard() metodu çağrıldı. Ekrana yazdırma işlemi gerçekleşti.
+
+            //string CountryCard1(string countryName, string capital, string flagColor)
+            //{
+            //    string cardKnow = $"Ülke: {countryName} / Başkent: {capital} / Bayrak Rengi: {flagColor}";
+            //    return cardKnow;
+            //}
+
+            //string CountryCard2(string countryName, string capital, string flagColor)
+            //{
+            //    return $"Ülke: {countryName} / Başkent: {capital} / Bayrak Rengi: {flagColor}";
+            //}
+
+            //Console.Write("Lütfen Ülke Adını Giriniz: ");
+            //string countryNameRead = Console.ReadLine();
+
+            //Console.Write("Lütfen Başkent Adını Giriniz: ");
+            //string capitalRead = Console.ReadLine();
+
+            //Console.Write("Lütfen Bayrak Rengini Giriniz: ");
+            //string flagColorRead = Console.ReadLine();
+
+            //Console.WriteLine(CountryCard1(countryNameRead, capitalRead, flagColorRead)); // Bu şekilde çağırırsak ekrana yazdırma işlemi gerçekleşmez. Çünkü geri dönüş değeri yok.   
+
+            //string read = CountryCard2(countryNameRead, capitalRead, flagColorRead); // Bu şekilde çağırırsak ekrana yazdırma işlemi gerçekleşmez. Çünkü geri dönüş değeri yok.   
+            //Console.WriteLine(read);  // başka bir yöntemle ekrana yazdırma işlemi gerçekleşir. Çünkü geri dönüş değeri var.
+
+            //Console.WriteLine(CountryCard1("Türkiye", "Ankara", "Kırmızı-Beyaz")); // Bu şekilde çağırırsak ekrana yazdırma işlemi gerçekleşir. Çünkü geri dönüş değeri var.
+
+
+
+            #endregion
+
+            #region Örnek Uygulama
+
+            //int ExamResult1(string student, int exam1, int exam2, int exam3) // Methodun geri dönüş tipi int. Yani bu method bir int değer döndürecek.
+            //{
+            //    int result = (exam1 + exam2 + exam3) / 3; // return ifadesi ile geri dönüş yapılır. return ifadesi ile methoddan çıkılır.
+            //    Console.WriteLine($"Öğrenci: {student} / Sınav Sonucu: {result}");
+            //    return result; // Geri dönüş değeri olarak result değişkenini döndürür.
+            //}
+
+            //ExamResult1("Furkan Öztürk", 80, 90, 100); // ExamResult() metodu çağrıldı. Ekrana yazdırma işlemi gerçekleşti. 
+
+            string ExamResult2(string student, int exam1, int exam2, int exam3) // Methodun geri dönüş tipi int. Yani bu method bir int değer döndürecek.
+            {
+                int result = (exam1 + exam2 + exam3) / 3;
+                if (result >= 50)
+                {
+                    return $"Öğrenci: {student} / Başarılı / Sınav Sonucu: {result}";
+                }
+                else
+                {
+                    return $"Öğrenci: {student} / Başarısız / Sınav Sonucu: {result}";
+                }
+            }
+            Console.WriteLine(ExamResult2("Furkan Öztürk", 80, 90, 100)); 
+            Console.WriteLine(ExamResult2("Furkan Öztürk", 20, 10, 5));
+
+
+
+
+
+
+
+
+
+
+
+            #endregion
+        }
+    }
+}
